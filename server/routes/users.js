7611c865b271bf0a31fe7f1d2f46605c7aaeba2c
Burhan <2019cs626@student.uet.edu.pk>
2022-07-27 19:50:13 +0500

    Completed the cron job module

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,7 @@ import express from "express";
 // const {sequelize}= require("sequelize");
 import { sequelize } from "../app/models";
 import axios from "axios";
+import nodeCron from "node-cron";
 
 var router = express.Router();
 
@@ -12,6 +13,51 @@ const config = {
   },
 };
 
+const scheduledTask = async () => {
+  const queryParentId = await sequelize.query(
+    "SELECT start_date FROM universal_data WHERE move_status = 'Booked' AND start_date = cast(now() as date)+1 group by start_date;"
+  );
+  const data = {
+    name: queryParentId[1].rows[0].start_date,
+    short_code: "",
+    billable: "no",
+    assigned_to_all: "yes",
+    billable_rate: "",
+  };
+  // const jobcodesApiData = await axios.post(
+  //   " https://rest.tsheets.com/api/v1/jobcodes",
+  //   {
+  //     data,
+  //   },
+  //   config
+  // );
+  // const parent_id = jobcodesApiData?.results?.jobcodes?.id || 00;
+  const parent_id = 10;
+  // SELECT title FROM Inventory_App.universal_data WHERE move_status = Booked and start_date = CURDATE() + 1;
+  const queryTitle = await sequelize.query(
+    "SELECT title FROM universal_data WHERE move_status = 'Booked' and start_date = cast(now() as date)+1;"
+  );
+  const payloadData = [];
+  queryTitle[1].rows.forEach((row) => {
+    const temp = {
+      name: row.title, //here please use title from query result above
+      parent_id,
+      short_code: "",
+      billable: "no",
+      assigned_to_all: "yes",
+      billable_rate: "",
+    };
+    payloadData.push(temp);
+  });
+  // const apiData = await axios.post(
+  //   " https://rest.tsheets.com/api/v1/jobcodes",
+  //   {
+  //     data: payloadData,
+  //   },
+  //   config
+  // );
+  return payloadData;
+};
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
   const queryParentId = await sequelize.query(
@@ -58,5 +104,13 @@ router.get("/", async function (req, res, next) {
   // );
   res.json({ data: payloadData });
 });
+
+const job = nodeCron.schedule(
+  "0 0 5 * * *",
+  async () => {
+    console.log(await scheduledTask());
+  },
+  { timezone: "US/Eastern" }
+);
 
 export default router;
